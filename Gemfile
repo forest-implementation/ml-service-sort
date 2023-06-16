98103b36ae9024563ce652cf601a7e7c42ec89e3
Adam Ulrich <a_ulrich@utb.cz>
2023-06-16 10:34:53 +0200

    init commit

---
+++ Gemfile
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in ml-service-sort.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
+
+gem "rubocop", "~> 1.21"
+
+group :test do
+  gem "minitest", "~> 5.0"
+  gem "ml-forest", git: "https://github.com/forest-implementation/ml-forest"
+end
---
+++ lib/ml/service/sort/quicksort.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative "version"
+
+module Ml
+  module Service
+    module Sort
+      class QuickSort
+        def split_point(data)
+          data.is_a?(Array) ? data.first : data
+        end
+
+        def decision(element, split)
+          element <=> split
+        end
+
+        def group(data, split)
+          { -1 => [], 0 => [], 1 => [] }.merge(data.group_by { |x| x <=> split })
+        end
+
+        def get_sample(data, _)
+          data
+        end
+
+        def end_condition(data)
+          data.all? { |x| x == data.first }
+        end
+      end
+    end
+  end
+end

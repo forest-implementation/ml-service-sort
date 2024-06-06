57e144fa282963f2578763d59edc17f0da5de7e4
Adam Ulrich <adamo.ulrich@gmail.com>
2024-06-06 14:23:13 +0200

    add function for prettify

--- test/ml/service/sort/test_quicksort.rb
+++ test/ml/service/sort/test_quicksort.rb
@@ -7,17 +7,11 @@ require "test_helper"
 
 require "ml/forest"
 class TestServiceQuicksort < Minitest::Test
-  def test_quick_sort_neighbours
-    input = [5, 8, 3, 4, 2, 7]
-    forest = Ml::Forest::Tree.new(input, trees_count: 1, forest_helper: Ml::Service::Sort::QuickSort.new)
-    res = forest.evaluate_forest(6).first
-    assert_equal res, [7]
-  end
 
   def test_quick_sort_sorting
     input = [5, 8, 3, 4, 2, 7]
     forest = Ml::Forest::Tree.new(input, trees_count: 1, forest_helper: Ml::Service::Sort::QuickSort.new)
-    res = forest.trees.first.to_a.flatten
-    assert_equal res, [2, 3, 4, 5, 7, 8]
+    res = forest.trees.first
+    assert_equal Ml::Service::Sort::QuickSort.prettify(res), [2, 3, 4, 5, 7, 8, nil]
   end
 end
